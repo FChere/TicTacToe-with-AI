01234b60f151ccee3e317328690fa75e76ce570e
cwanfeng <cwanfeng@uw.edu>
2022-11-15 01:18:23 -0800

    New Commit

--- thegame.py
+++ thegame.py
@@ -32,7 +32,7 @@ class Game:
             if 0 <= self.coordinate_x <= 2 and 0 <= self.coordinate_y <= 2:
                 input_is_valid = True
             else:
-                print("Invalid Input! Please try again for integers between 0 and 2.")
+                print("Wrong move, please try again")
 
 
 class SinglePlayerGame(Game):
@@ -43,26 +43,23 @@ class SinglePlayerGame(Game):
     def AI_random_step(self):
         empty_space = []
 
-        # See how much space left
         for i in range(3):
             for j in range(3):
                 if self.board[i][j] is None:
                     empty_space.append((i, j))
 
-        # Get a random space and take a step
         step_corrdinate = empty_space[random.randint(0, len(empty_space) - 1)]
 
         return step_corrdinate
 
 
     def game_loop(self):
-        # print(self.board)
         print_board(self.board)
         while self.winner is None:
             if self.is_user_turn == True:
-                # Print the current board
-                print("==================================")
-                print("User's Turn, please input")
+                
+                print("")
+                print("Your Turn, please make a move")
 
                 # Input a move from the player
                 self.input_corrdinate()
@@ -70,14 +67,12 @@ class SinglePlayerGame(Game):
                 # Update the board
                 self.update_board(self.coordinate_x, self.coordinate_y, 'X')
 
-                # Print the board
-                print("Board after Your input:")
                 print_board(self.board)
 
             else:
                 # Print the current board
-                print("==================================")
-                print("Computer AI's Turn, please wait...")
+                print("")
+                print("AI making a move, please wait...")
                 time.sleep(3)
 
                 # AI Bot take a step
@@ -86,8 +81,6 @@ class SinglePlayerGame(Game):
                 # Update the board
                 self.update_board(AI_step[0], AI_step[1], 'O')
 
-                # Print board
-                print("current board below:")
                 print_board(self.board)
 
             # Get Winner
@@ -96,7 +89,7 @@ class SinglePlayerGame(Game):
             # Switch Player
             self.is_user_turn = 1 - self.is_user_turn
 
-        print("Winner is", self.winner)
+        print("The winner is", self.winner)
 
 
 class MultiPlayerGame(Game):
@@ -108,8 +101,8 @@ class MultiPlayerGame(Game):
         print_board(self.board)
 
         while self.winner is None:
-            print("====================================")
-            print("It's player", self.current_player, "turn, please input")
+            print("")
+            print(self.current_player, "please make a move")
 
             # Input a move from the player
             self.input_corrdinate()
@@ -124,4 +117,4 @@ class MultiPlayerGame(Game):
             # Switch player
             self.current_player = other_player(self.current_player)
 
-        print("Winner is", self.winner)
+        print("The winner is", self.winner)
