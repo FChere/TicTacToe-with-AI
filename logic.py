571a53776311b5d166243180ed15419b19d89812
cwanfeng <cwanfeng@uw.edu>
2022-11-15 01:23:25 -0800

    New Commit

--- logic.py
+++ logic.py
@@ -16,40 +16,37 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    O_win = 0  # How many times O won totally
-    X_win = 0  # How many times X won totally
+    O_win = 0  
+    X_win = 0  
 
-    # Look at Row First
     for row in board:
         count_O = 0
         count_X = 0
         for item in row:
             if item == 'O':
-                count_O = count_O + 1  # Count how many O in a row
+                count_O = count_O + 1  # Check how many O in a row
             elif item == 'X':
-                count_X = count_X + 1  # Count how many X in a row
+                count_X = count_X + 1  # Check how many X in a row
         if count_O == 3:
-            O_win = O_win + 1  # If there are 3 O in a row, O won 1 time
+            O_win = O_win + 1  # If there are 3 O in a row, O won 
         elif count_X == 3:
-            X_win = X_win + 1  # If there are 3 X in a row, X won 1 time
+            X_win = X_win + 1  # If there are 3 X in a row, X won 
 
 
-    # Look at Col
-    board_trans = np.transpose(board)  # transpose the matrix to better iteration
+    board_trans = np.transpose(board) 
     for row in board_trans:
         count_O = 0
         count_X = 0
         for item in row:
             if item == 'O':
-                count_O = count_O + 1  # Count how many O in a col
+                count_O = count_O + 1  # Check how many O in a col
             elif item == 'X':
-                count_X = count_X + 1  # Count how many X in a col
+                count_X = count_X + 1  # Check how many X in a col
         if count_O == 3:
-            O_win = O_win + 1  # If there are 3 O in a col, O won 1 time
+            O_win = O_win + 1  # If there are 3 O in a col, O won 
         elif count_X == 3:
-            X_win = X_win + 1  # If there are 3 X in a row, X won 1 time
+            X_win = X_win + 1  # If there are 3 X in a row, X won 
 
-    # Look at diag
     count_O = 0
     count_X = 0
     for i in range(3):
