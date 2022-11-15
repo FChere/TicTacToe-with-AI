776b7a12e8d915ff02addf90f8574e564417e142
cwanfeng <cwanfeng@uw.edu>
2022-11-15 12:32:45 -0800

    New Commit

--- logic.py
+++ logic.py
@@ -4,12 +4,14 @@
 
 import numpy as np
 
+
 def make_empty_board():
     return [
         [None, None, None],
         [None, None, None],
         [None, None, None],
     ]
+
 
 def get_winner(board):
     """Determines the winner of the given board.
@@ -81,10 +83,10 @@ def get_winner(board):
     else:
         return None
 
+
 def other_player(player):
     """Given the character for a player, returns the other player."""
     if player == 'X':
         return 'O'
     else:
         return 'X'
-
